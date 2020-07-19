fee4a9f3e8aa3a881eb10a8d3476d258d1090557
Tzahile <Tzahile@users.noreply.github.com>
2020-07-19 19:03:56 +0300

    made bar more compact

--- projCode/src/components/appBar.vue
+++ projCode/src/components/appBar.vue
@@ -2,14 +2,14 @@
   <v-app-bar
     flat
     prominent
-    :height="!$vuetify.breakpoint.xs ? 220 : 168"
+    :height="!$vuetify.breakpoint.xs ? 200 : 160"
     app
     color="primary"
     dark
   >
-    <v-row no-gutter :align="$vuetify.breakpoint.xs ? 'center' : 'end'">
+    <v-row no-gutter align="end">
       <v-col align-self="start" class="pa-0" cols="auto">
-        <v-avatar tile size="160">
+        <v-avatar tile size="150">
           <v-img src="../assets/cvFinal.jpeg"></v-img>
         </v-avatar>
       </v-col>
@@ -19,29 +19,32 @@
         xs="3"
         md="8"
         sm="8"
+        align="start"
         :class="{ 'px-0': $vuetify.breakpoint.xs }"
       >
         <v-list-item>
           <v-list-item-content>
             <v-list-item-title
-              class="text-xl-h4 text-lg-h4 text-md-h4 text-sm-h4 text-h5"
+              class="text-xl-h5 text-lg-h5 text-md-h5 text-sm-h5 text-h6"
             >
               Izhak Lehmann
             </v-list-item-title>
-            <v-list-item-subtitle class="text-sm-h4 text-subtitle-1">
+            <v-list-item-subtitle
+              class="text-xl-h6 text-lg-h6 text-md-h6 text-sm-h6 text-subtitle-1"
+            >
               Electrical Engineer
             </v-list-item-subtitle>
           </v-list-item-content>
         </v-list-item>
-        <v-col v-if="$vuetify.breakpoint.xs">
+        <v-col v-if="$vuetify.breakpoint.xs" class="py-0">
           <profLinks></profLinks>
         </v-col>
       </v-col>
-      <v-col v-if="!$vuetify.breakpoint.xs" cols="4" xs="2">
+      <v-col v-if="!$vuetify.breakpoint.xs" class="py-0" cols="4" xs="2">
         <profLinks></profLinks>
       </v-col>
       <v-spacer></v-spacer>
-      <v-col v-if="!$vuetify.breakpoint.xs" md="5" align="end">
+      <v-col v-if="!$vuetify.breakpoint.xs" class="py-0" md="5" align="end">
         <v-btn text href="mailto:gotenks33@gmail.com" class="text-lowercase">
           <v-icon small class="pr-2">mdi-gmail</v-icon>
           gotenks33@gmail.com
